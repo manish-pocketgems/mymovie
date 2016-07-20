79c95994fe38d3dc690d75822ffab669c7de3f0c
Eko Wibowo <eko@pocketgems.com>
2016-07-20 14:24:26 -0700

    code refactor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import json
 import logging
-import urllib2
+import urllib
 import webapp2
 
 from google.appengine.api import memcache, urlfetch
@@ -10,8 +10,8 @@ from google.appengine.ext import ndb
 from helper import write_mako_template
 
 # API related constants
-TMDB_API_SEARCH_URL = 'http://api.themoviedb.org/3/search/movie?query='
-TMDB_API_VIDEO_URL = 'http://api.themoviedb.org/3/movie/%s/videos?api_key=%s'
+TMDB_API_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie'
+TMDB_API_VIDEO_URL = 'https://api.themoviedb.org/3/movie/%s/videos?api_key=%s'
 API_KEY = 'cb6b724d63906d290169b30bdd60c3a1'
 
 # Memcache related constants
@@ -22,12 +22,14 @@ MEMCACHE_TIMEOUT = 30
 
 # Movie model definition
 class Movie(ndb.Model):
+    # disable builtin memcache and context cache
     _use_cache = _use_memcache = False
     title = ndb.StringProperty(indexed=True, required=True)
     overview = ndb.TextProperty(indexed=True, required=True)
     trailer = ndb.StringProperty(indexed=True, required=True)
     poster = ndb.StringProperty(indexed=True, required=True)
-    average_rating = ndb.IntegerProperty(indexed=True, default=0)
+    average_rating = ndb.ComputedProperty(lambda self: self.calc_avg_rating(),
+                                          indexed=True)
     total_rating = ndb.IntegerProperty(indexed=True, default=0)
     num_ratings = ndb.IntegerProperty(indexed=True, default=0)
 
@@ -42,6 +44,37 @@ class Movie(ndb.Model):
                                 MEMCACHE_TIMEOUT):
                 logging.error('Memcache add failed')
         return movies
+
+    @property
+    def movie_id(self):
+        return self.key.id()
+
+    def incr_rating(self, rating):
+        """Increment the rating for a movie"""
+        self.total_rating += int(rating)
+        self.num_ratings += 1
+
+    def _post_put_hook(self, future):
+        """Hook that runs after put()"""
+        mc_key = MEMCACHE_KEY_PREFIX + str(self.movie_id)
+        memcache.delete(mc_key)
+
+    @classmethod
+    def get_from_cache(cls, movie_id):
+        """Get an entity from cache layer"""
+        mc_key = MEMCACHE_KEY_PREFIX + str(movie_id)
+        movie = memcache.get(mc_key)
+        if movie is None:
+            movie = Movie.get_by_id(int(movie_id))
+            if movie:
+                memcache.add(mc_key, movie, MEMCACHE_TIMEOUT)
+        return movie
+
+    def calc_avg_rating(self):
+        """Calculate avg rating"""
+        if self.num_ratings == 0:
+            return 0
+        return self.total_rating / self.num_ratings
 
 
 # Handler for the homepage
@@ -67,11 +100,14 @@ class SubmitMovieHandler(webapp2.RequestHandler):
 
         movie = Movie.query().filter(Movie.title == title).get()
         if not movie:
-            if not memcache.delete(MEMCACHE_ALL_MOVIES_KEY):
-                logging.error("Memcache delete failed")
-            movie = Movie(title=title, overview=overview, trailer=trailer_url,
+            movie = Movie(title=title,
+                          overview=overview,
+                          trailer=trailer_url,
                           poster=poster)
             movie.put()
+            if memcache.delete(MEMCACHE_ALL_MOVIES_KEY) == memcache.DELETE_NETWORK_FAILURE:
+                logging.info('deleting memcache all movies key failed')
+            logging.info('deleting memcache all movies key')
         self.redirect('/')
 
 
@@ -79,36 +115,39 @@ class SearchHandler(webapp2.RequestHandler):
     PATH = '/search'
 
     def get(self):
-        movie_name = self.request.get('movie_name')
-        movie_name = urllib2.quote(movie_name)
-        url = TMDB_API_SEARCH_URL + movie_name + '&api_key=' + API_KEY
+        YOUTUBE_EMBED_URL = "https://www.youtube.com/embed/"
+        movie_name = self.request.get('movie_name').strip()
+        params = dict(query=movie_name, api_key=API_KEY)
+        url = TMDB_API_SEARCH_URL + "?" + urllib.urlencode(params)
         try:
-            resp = urlfetch.fetch(url)
+            resp = urlfetch.fetch(url, validate_certificate=True)
             if resp.status_code != 200:
                 raise ValueError("Error response: %s" % resp.code)
             res = json.loads(resp.content)
             results = []
             for movie_info in res.get('results'):
-                title = movie_info.get('title') or movie_info.get('original_title')
+                title = (movie_info.get('title') or
+                         movie_info.get('original_title'))
                 overview = movie_info.get('overview')
                 poster_path = movie_info.get('poster_path')
                 if not poster_path:
                     continue
-                poster_path = 'http://image.tmdb.org/t/p/w500' + poster_path
+                poster_path = 'https://image.tmdb.org/t/p/w500' + poster_path
                 movie_id = movie_info.get('id')
 
                 # Get trailer url from imdb
                 movie_url = TMDB_API_VIDEO_URL % (movie_id, API_KEY)
                 logging.info('MOVIE URL : %s\n' % movie_url)
-                resp = urlfetch.fetch(movie_url)
+                resp = urlfetch.fetch(movie_url, validate_certificate=True)
                 if resp.status_code != 200:
                     continue
                 resp = json.loads(resp.content)
                 resp = resp.get('results', [])
                 if not resp:
                     continue
-                trailer_url = "http://www.youtube.com/embed/" + resp[0].get('key')
-                info = dict(title=title, overview=overview, poster=poster_path,
+                trailer_url = YOUTUBE_EMBED_URL + resp[0].get('key')
+                info = dict(title=title, overview=overview,
+                            poster=poster_path,
                             trailer_url=trailer_url)
                 results.append(info)
             if results:
@@ -118,22 +157,19 @@ class SearchHandler(webapp2.RequestHandler):
                 write_mako_template(self, 'templates/submit.html',
                                     error="No results found")
 
-        except (ValueError, urllib2.URLError) as e:
-            logging.error('Error retrieving movie list : \n%s' % e)
+        except urlfetch.Error as e:
+            logging.exception(e)
+        except ValueError as e:
+            logging.exception(e)
 
 
 class ViewMovieHandler(webapp2.RequestHandler):
     PATH = "/movie/([0-9]+)"
 
     def get(self, movie_id):
-        mc_key = MEMCACHE_KEY_PREFIX + movie_id
-        movie = memcache.get(mc_key)
+        movie = Movie.get_from_cache(movie_id)
         if not movie:
-            movie = Movie.get_by_id(int(movie_id))
-            if movie:
-                memcache.add(mc_key, movie, MEMCACHE_TIMEOUT)
-            else:
-                self.response.write('Movie not found')
+                return self.response.write('Movie not found')
 
         write_mako_template(self, 'templates/view_movie.html', movie=movie)
 
@@ -144,17 +180,8 @@ class ViewMovieHandler(webapp2.RequestHandler):
 
         # Update rating data
         movie = Movie.get_by_id(int(movie_id))
-        num_ratings = movie.num_ratings + 1
-        total_rating = movie.total_rating + int(rating)
-        average_rating = total_rating / num_ratings
-        movie.num_ratings = num_ratings
-        movie.total_rating = total_rating
-        movie.average_rating = average_rating
+        movie.incr_rating(rating)
         movie.put()
-
-        # Evict from memcache
-        mc_key = MEMCACHE_KEY_PREFIX + movie_id
-        memcache.delete(mc_key)
 
         self.redirect('/')
 
